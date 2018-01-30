9d798c1a799bf26115951e851c6439642b60cf5f
ben <wbpiggot@gmail.com>
2018-01-29 18:01:39 -0800

    Typescript compiling process now working

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute } from 'react-router';
+import { Router, Route } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import store from './store';
+import store from './store.js';
 import AppContainer from './containers/AppContainer';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
+import injectTapEventPluginRequire = require("react-tap-event-plugin");
+injectTapEventPluginRequire();
 
-class App extends Component {
+class App extends React.Component<{}, {}> {
   render () {
     return (
       <Provider store={store}>
